28064228490bacabe6405e3b0dbda6b3c0ead41f
Weiyanyu <chinayeonon@gmail.com>
2018-04-02 19:09:21 +0800

    init project

---
+++ src/main/java/top/yeonon/lmmall/LmMallApplication.java
@@ -0,0 +1,12 @@
+package top.yeonon.lmmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmMallApplication.class, args);
+	}
+}
