1b6c46085fc068ef29aecf6ddf6de80b1bc43a10
Weiyanyu <chinayeonon@gmail.com>
2018-04-11 22:17:06 +0800

    add some commont

--- src/main/java/top/yeonon/lmmall/token/JwtTokenGenerator.java
+++ src/main/java/top/yeonon/lmmall/token/JwtTokenGenerator.java
@@ -15,6 +15,8 @@ import java.util.Map;
 /**
  * @Author yeonon
  * @date 2018/4/11 0011 20:04
+ *
+ * 基于JWT 实现的token生成和校验
  **/
 @Component
 public class JwtTokenGenerator implements TokenGenerator<String> {
@@ -25,28 +27,35 @@ public class JwtTokenGenerator implements TokenGenerator<String> {
 
     @Override
     public String generate(String authorization) throws Exception {
+        //token签发时间
         Date iatDate = new Date();
+        //设置超时日期（是日期，不是时间）
         Calendar nowTime = Calendar.getInstance();
         nowTime.add(Calendar.MINUTE, coreProperties.getSecurity().getTokenExpire());
         Date expireTime = nowTime.getTime();
 
+        //这个map是 jwt的头部
         Map<String, Object> map = Maps.newHashMap();
         map.put("alg", "HS256");
         map.put("typ", "JWT");
         String token = JWT.create()
-                          .withHeader(map)
-                          .withClaim("authorization", authorization)
-                          .withExpiresAt(expireTime)
-                          .withIssuedAt(iatDate)
-                          .sign(Algorithm.HMAC256(SECRET));
+                          .withHeader(map)  //插入头部
+                          .withClaim("authorization", authorization)  //插入负载，即数据
+                          .withExpiresAt(expireTime)    //超期日期，必须大于签发日期
+                          .withIssuedAt(iatDate)        //签发日期
+                          .sign(Algorithm.HMAC256(SECRET));  //使用HMAC256算法
         return token;
     }
+
 
     @Override
     public boolean verifyToken(String token) throws Exception {
         Algorithm algorithm = Algorithm.HMAC256(SECRET);
+        //创建校验器
         JWTVerifier verifier = JWT.require(algorithm).build();
         try {
+            //验证token是否合法，合法返回负载（数据），否则抛出异常
+            //这里不能用是否为null来判断是否合法，返回值永远是非NULL
             verifier.verify(token);
         } catch (JWTVerificationException e) {
             return false;
--- src/main/java/top/yeonon/lmmall/token/TokenGenerator.java
+++ src/main/java/top/yeonon/lmmall/token/TokenGenerator.java
@@ -4,11 +4,19 @@ package top.yeonon.lmmall.token;
 /**
  * @Author yeonon
  * @date 2018/4/11 0011 19:40
+ *
+ * Token生成器
  **/
 public interface TokenGenerator<T> {
 
+    /**
+     * 生成token
+     */
     String generate(T authorization) throws Exception;
 
+    /**
+     * 验证token是否合法
+     */
     boolean verifyToken(String token) throws Exception;
 
 }
