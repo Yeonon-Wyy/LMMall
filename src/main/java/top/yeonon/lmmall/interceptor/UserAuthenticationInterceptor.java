15b866c0aaf4de6ae2264bdd3b6a949555f5cbe2
Weiyanyu <chinayeonon@gmail.com>
2018-04-11 19:07:52 +0800

    add some commont

--- src/main/java/top/yeonon/lmmall/interceptor/UserAuthenticationInterceptor.java
+++ src/main/java/top/yeonon/lmmall/interceptor/UserAuthenticationInterceptor.java
@@ -1,6 +1,7 @@
 package top.yeonon.lmmall.interceptor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
@@ -25,6 +26,7 @@ import java.lang.reflect.Method;
  * @date 2018/4/11 0011 14:34
  **/
 @Component
+@Log
 public class UserAuthenticationInterceptor implements HandlerInterceptor {
 
     private final static String CONSUMER = "CONSUMER";
@@ -36,12 +38,22 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
     @Autowired
     private IUserService userService;
 
+    /**
+     * 在进入URL Mapping对应的处理方法之前
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("拦截器拦截，拦截的URL是 " + request.getRequestURI());
         HttpSession session = request.getSession();
+        //默认是isPass,因为这个拦截器是全局拦截器，会拦截所有的URL，但是并不是所有的URL都需要验证用户或者管理员
+        //所以不需要认证的默认就是通过的
         boolean isPass = true;
+        //handler就是HandlerMethod，但是Spring传进来的是Object，需要转换才能调用API
         HandlerMethod handlerMethod = (HandlerMethod) handler;
+        //获取被拦截的方法（即Url Mapping对应的处理方法）
         Method method = handlerMethod.getMethod();
+
+        //方便下面判断本次登录的是哪种类型，然后做出判断
         String permissionType = CONSUMER;
         if (method.getAnnotation(Consumer.class) != null) {
             isPass = isConsumer(session);
@@ -52,34 +64,30 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
             permissionType = MANAGER;
         }
 
+        //如果isPass 是false,也就是没有通过验证，在本项目中就是没有登录如何没有相应的权限
         if (!isPass) {
             ServerResponse serverResponse = null;
+            //根据PermissionType 的不同返回不同的内容
             if (permissionType.equals(CONSUMER)) {
                 serverResponse = ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户需要登录");
             }
             else {
                 serverResponse = ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "管理员需要登录");
             }
+            //将内容写到response中返回给调用者
             response.setContentType("application/json");
             response.setHeader("Access-Control-Allow-Credentials", "true");
             response.setCharacterEncoding("UTF-8");
             PrintWriter writer = response.getWriter();
             writer.write(objectMapper.writeValueAsString(serverResponse));
         }
-
-
+        //最后如果是true，那么会继续走下面的流程（访问资源或者后面的拦截器，过滤器等）
+        //如果是false,那么就会将response返回。
         return isPass;
     }
 
     private boolean isManager(HttpSession session) {
-        User user = (User) session.getAttribute(ServerConst.SESSION_KEY_FOR_CURRENT);
-        if (user == null) {
-            return false;
-        }
-        return true;
-    }
 
-    private boolean isConsumer(HttpSession session) {
         User user = (User) session.getAttribute(ServerConst.SESSION_KEY_FOR_CURRENT);
         if (user == null) {
             return false;
@@ -89,14 +97,28 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
             return false;
         }
         return true;
-
     }
 
+    private boolean isConsumer(HttpSession session) {
+        User user = (User) session.getAttribute(ServerConst.SESSION_KEY_FOR_CURRENT);
+
+        if (user == null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 在完成URL Mapping对应的处理方法之后，视图渲染之前
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
     }
 
+    /**
+     * 在视图渲染之后
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
