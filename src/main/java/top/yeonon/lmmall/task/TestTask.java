de7a7c0c38d9887e90819f9682fdf66a8e7b3f89
Weiyanyu <chinayeonon@gmail.com>
2018-04-17 15:04:22 +0800

    spring scheduled task

---
+++ src/main/java/top/yeonon/lmmall/task/TestTask.java
@@ -0,0 +1,19 @@
+package top.yeonon.lmmall.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @Author yeonon
+ * @date 2018/4/17 0017 15:01
+ **/
+@Component
+public class TestTask {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void TimePrint() {
+        System.out.println(new Date());
+    }
+}
