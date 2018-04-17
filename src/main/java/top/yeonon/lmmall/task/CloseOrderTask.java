0abc01485bb33df0a14fc3158bec141d3bfa4e32
Weiyanyu <chinayeonon@gmail.com>
2018-04-17 17:39:11 +0800

    redis lock

--- src/main/java/top/yeonon/lmmall/task/CloseOrderTask.java
+++ src/main/java/top/yeonon/lmmall/task/CloseOrderTask.java
@@ -39,18 +39,36 @@ public class CloseOrderTask {
             log.info("获取锁成功,开始执行任务");
             closeOrder(2);
         } else {
-            log.info("获取锁失败，放弃任务");
+            //并不直接放弃，先获得设置锁的时候放入的时间戳
+            String lockStr = (String) redisTemplate.opsForValue().get("CLOSE_ORDER_LOCK");
+            //如果这个时间戳不为null且当前时间已经大于了原来的时间，此时就表示可以去抢夺资源了
+            //这也就防止了某个资源被同一个进程持续持有
+            if (lockStr != null && System.currentTimeMillis() > Long.parseLong(lockStr)) {
+
+                //使用getSet方法来设置新值并返回旧值，如果旧值为null，表示可以获取锁了
+                //如果不为null，但是旧值和lockStr相同，就说明期间没有其他线程来设置值，即没有其他线程抢先一步获取锁
+                //那么，当前线程就有权利获取锁，这又防止了死锁
+                String getSetResult = (String) redisTemplate.opsForValue().getAndSet("CLOSE_ORDER_LOCK",
+                        String.valueOf(System.currentTimeMillis()) + timeOut);
+                if (getSetResult == null || getSetResult.equals(lockStr)) {
+                    closeOrder(2);
+                } else {
+                    log.info("获取锁失败，放弃任务");
+                }
+            } else {
+                log.info("获取锁失败，放弃任务");
+            }
         }
     }
 
     private void closeOrder(int hour) {
         boolean isTrue = redisTemplate.expire("CLOSE_ORDER_LOCK", 50000, TimeUnit.MILLISECONDS);
         //orderService.closeOrder(hour);
-        try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(50);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         redisTemplate.delete("CLOSE_ORDER_LOCK");
         log.info(Thread.currentThread().getName() + "释放锁");
     }
